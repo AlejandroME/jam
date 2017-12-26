757c32e14cce0796618906d896aa9ba6a1c43adf
Leandro Bolivar <leandrob131@gmail.com>
2017-12-26 13:14:16 -0500

    Added print function to Yaml

--- jam-parser/src/main/scala/jam/Yaml.scala
+++ jam-parser/src/main/scala/jam/Yaml.scala
@@ -1,35 +1,37 @@
 package jam
 
+import jam.parser.YamlPrinter
+
 import scala.collection.immutable.ListMap
 
 sealed abstract class Yaml extends Product with Serializable {
 
-  def isPrimitive: Boolean = false
+  def isPrimitive: Boolean = true
+
+  def print: String =
+    YamlPrinter.printYaml(this, new StringBuilder).toString()
 }
 
 object Yaml {
 
-  private[jam] final case object YNull extends Yaml {
-    override def isPrimitive: Boolean = true
+  private[jam] final case object YNull extends Yaml
+
+  private[jam] final case object YTrue extends Yaml
+
+  private[jam] final case object YFalse extends Yaml
+
+  private[jam] final case class YBigDecimal(v: BigDecimal) extends Yaml
+
+  private[jam] final case class YString(v: String) extends Yaml
+
+  private[jam] final case class YArray(v: Vector[Yaml]) extends Yaml {
+    override def isPrimitive: Boolean = false
   }
 
-  private[jam] final case object YTrue extends Yaml {
-    override def isPrimitive: Boolean = true
+  private[jam] final case class YMap(v: ListMap[String, Yaml]) extends Yaml {
+    override def isPrimitive: Boolean = false
+
+    def ++(m: YMap): YMap =
+      YMap(v ++ m.v)
   }
-
-  private[jam] final case object YFalse extends Yaml {
-    override def isPrimitive: Boolean = true
-  }
-
-  private[jam] final case class YBigDecimal(v: BigDecimal) extends Yaml {
-    override def isPrimitive: Boolean = true
-  }
-
-  private[jam] final case class YString(v: String) extends Yaml {
-    override def isPrimitive: Boolean = true
-  }
-
-  private[jam] final case class YArray(v: Vector[Yaml]) extends Yaml
-
-  private[jam] final case class YMap(v: ListMap[String, Yaml]) extends Yaml
 }
